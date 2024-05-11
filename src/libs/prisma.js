344b4522f7c45ed70a54279700f80c4ec50b1989
roboticswarp <roboticswarpdesarrollador@gmail.com>
2024-05-11 12:02:04 -0500

    corrigiendo-prismpa

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,3 +1,11 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient()
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export { prisma };
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
