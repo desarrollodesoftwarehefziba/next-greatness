57cf635ec8ff357cf5619b63f810fd63d8c9a692
roboticswarp <roboticswarpdesarrollador@gmail.com>
2024-05-11 14:42:43 -0500

    filtros

--- src/components/medicoCarta.jsx
+++ src/components/medicoCarta.jsx
@@ -1,17 +1,42 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-function MedicoCarta({ medico }) {
+
+function MedicoCarta({ medicos }) {
+  const [busqueda, setBusqueda] = useState("");
   const router = useRouter();
+
+  const handleSearch = (e) => {
+    setBusqueda(e.target.value);
+  };
+
+  const medicosFiltrados = medicos.filter(
+    (medico) =>
+      medico.nombre.toLowerCase().includes(busqueda.toLowerCase()) ||
+      medico.especialidad.toLowerCase().includes(busqueda.toLowerCase())
+  );
+
   return (
-    <div className="bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer"
-    onClick={() => {
-        router.push('/doctors/' + medico.id)
-    }}
-    >
-      <h3 className="font-bold text-2xl mb-2">{medico.nombre}</h3>
-      <h3>{medico.especialidad}</h3>
-      <p>{medico.biografia}</p>
+    <div>
+      <input
+        type="text"
+        value={busqueda}
+        onChange={handleSearch}
+        placeholder="Buscar por nombre o especialidad"
+      />
+      {medicosFiltrados.map((medico) => (
+        <div
+          key={medico.id}
+          className="bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer"
+          onClick={() => {
+            router.push("/doctors/" + medico.id);
+          }}
+        >
+          <h3 className="font-bold text-2xl mb-2">{medico.nombre}</h3>
+          <h3>{medico.especialidad}</h3>
+          <p>{medico.biografia}</p>
+        </div>
+      ))}
     </div>
   );
 }
